328ad3323d2824a715ca23afaeae618df9153eff
yoavk-sudo <kenkendler123@gmail.com>
2025-09-06 22:14:52 +0300

    Update terrain and scene settings, add lighting asset
    Updated Terrain_SceneA prefab and Scene_A/Scene_B Unity scenes to newer serialization formats and settings. Added Scene_BSettings.lighting asset and meta, modified terrain and lightmap settings, and improved Barrel Manager Trigger integration in Scene_A. Also updated TriggerManager.cs and Scene_B_Terrain.asset.

--- Assets/Arctic_Castle/Scripts/TriggerManager.cs
+++ Assets/Arctic_Castle/Scripts/TriggerManager.cs
@@ -4,25 +4,10 @@ using UnityEngine;
 
 public class TriggerManager : MonoBehaviour
 {
-    void OnTriggerStay(Collider other)
+    [SerializeField] GameObject barrelManagerObj;
+    private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.name!= "FpsController")
-        {
-            return;
-        }
-        // No tag check or layer filtering
-        GameObject obj = other.gameObject;
-
-        // GC Alloc: new string every frame
-        Debug.Log("Something is inside the trigger: " + obj.name);
-
-        // GC Alloc: create a temp list (for no reason)
-        List<string> dummy = new List<string>();
-        dummy.Add("triggered");
-        dummy.Add(obj.tag);
-
-        // Redundant enable of manager every frame
-        GameObject barrelManagerObj = GameObject.Find("BarrelManager");
+        if (other.GetComponent<CharController_Motor>() == null) return;
         if (barrelManagerObj != null)
         {
             BarrelManager bm = barrelManagerObj.GetComponent<BarrelManager>();
@@ -31,5 +16,34 @@ public class TriggerManager : MonoBehaviour
                 bm.enabled = true;
             }
         }
+        Destroy(gameObject);
     }
+    //void OnTriggerStay(Collider other)
+    //{
+    //    if(other.gameObject.name!= "FpsController")
+    //    {
+    //        return;
+    //    }
+    //    // No tag check or layer filtering
+    //    GameObject obj = other.gameObject;
+
+    //    // GC Alloc: new string every frame
+    //    Debug.Log("Something is inside the trigger: " + obj.name);
+
+    //    // GC Alloc: create a temp list (for no reason)
+    //    List<string> dummy = new List<string>();
+    //    dummy.Add("triggered");
+    //    dummy.Add(obj.tag);
+
+    //    // Redundant enable of manager every frame
+    //    GameObject barrelManagerObj = GameObject.Find("BarrelManager");
+    //    if (barrelManagerObj != null)
+    //    {
+    //        BarrelManager bm = barrelManagerObj.GetComponent<BarrelManager>();
+    //        if (bm != null)
+    //        {
+    //            bm.enabled = true;
+    //        }
+    //    }
+    //}
 }
