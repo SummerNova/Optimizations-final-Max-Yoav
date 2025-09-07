51e334dc1be15d0ce12571004863402fbf2e1077
yoavk-sudo <kenkendler123@gmail.com>
2025-09-07 19:50:00 +0300

    Activate multiple scene objects and update parents
    Set m_IsActive to 1 for numerous prefab instances in Scene_A.unity, enabling their visibility or interaction. Updated m_TransformParent references for many objects to fileID 2111786616, and made minor transform adjustments to select prefabs. These changes likely support new gameplay or level design requirements.

--- Assets/Arctic_Castle/Scripts/TriggerCell.cs
+++ Assets/Arctic_Castle/Scripts/TriggerCell.cs
@@ -12,26 +12,32 @@ public class TriggerCell : MonoBehaviour {
 		if (playerStartsInsideCell) {			
 			objectsInsideCell.SetActive (true);
 			objectsOutsideCell.SetActive (false);
+			Debug.Log("deactivated shore");
 		}
 
 		if (!playerStartsInsideCell) {
 			objectsInsideCell.SetActive (false);
 			objectsOutsideCell.SetActive (true);
-		}
-		
-	}
+			Debug.Log("deactivated castle");
+        }
+
+    }
 
 
 	void OnTriggerEnter(Collider other){
-		objectsInsideCell.SetActive (true);
+        if (other.GetComponent<CharController_Motor>() == null) return;
+        objectsInsideCell.SetActive (true);
 		objectsOutsideCell.SetActive (false);
-		
-	}
+		Debug.Log("activated castle");
+
+    }
 
 	void OnTriggerExit(Collider other){
-		objectsInsideCell.SetActive (false);
+        if (other.GetComponent<CharController_Motor>() == null) return;
+        objectsInsideCell.SetActive (false);
 		objectsOutsideCell.SetActive (true);
-	}
+		Debug.Log("smth");
+    }
 
 
 }
